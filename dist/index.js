fdf4add530c124067fde1286fdd7e08a12100552
Cheese-strat <46002480+Cheese-strat@users.noreply.github.com>
2021-02-23 21:05:20 +0800

    adding other responses depending on headers

--- dist/index.js
+++ dist/index.js
@@ -6,7 +6,16 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = express_1.default();
 const PORT = 527;
-app.get("/", (_req, res) => {
-    res.send("<p>hello world</p>");
+app.get("/", (req, res) => {
+    const userAgent = req.headers["user-agent"];
+    const accept = req.headers["accept"];
+    if (!userAgent) {
+        return res.send('{code:200,message:"hello world"}');
+    }
+    if (!accept) {
+        return res.send('{code:400,message:"no accept header supplied"}');
+    }
+    console.log("user agent: ", userAgent);
+    return res.send("<p>hello world</p>");
 });
 app.listen(PORT);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
 import express from "express";
 const app = express();
 const PORT = 527;
-app.get("/", (_req, res) => {
-	res.send("<p>hello world</p>");
+
+app.get("/", (req, res) => {
+	const userAgent = req.headers["user-agent"];
+	const accept = req.headers["accept"];
+	if (!userAgent) {
+		return res.send('{code:200,message:"hello world"}');
+	}
+	if (!accept) {
+		return res.send('{code:400,message:"no accept header supplied"}');
+	}
+	console.log("user agent: ", userAgent);
+	return res.send("<p>hello world</p>");
 });
 app.listen(PORT);
